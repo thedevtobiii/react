6eb6ec970c780e815e36bb2bc7cce2e28aad26ac
DevTobi <fariuoyetola11@gmail.com>
2025-01-29 05:23:19 +0100

    react facts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>this is react</h1>
-    </>
-  )
-}
-
-export default App
+import { createRoot } from 'react-dom/client'
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <main>
+    <img src="img/React-icon.png" width="40px" alt='React Logo' />
+    <h1>Fun Facts about React js</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>was originally created by Jordan Walke</li>
+      <li>Has over 200k stars on Github</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise app, including mobile apps</li>
+    </ul>
+  </main>
+)
+// root.render can not render 2 element side by side, needs a parent element
